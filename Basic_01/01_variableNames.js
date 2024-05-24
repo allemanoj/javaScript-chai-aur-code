b8901fbc632ba3e227c4b4d59537588478d207e1
Alle Manoj <123374919+allemanoj@users.noreply.github.com>
2024-05-24 06:13:28 +0000

    datatypes

---
+++ Basic_01/01_variableNames.js
@@ -0,0 +1,14 @@
+const noChange = 1205;
+let peru  = "manoj";
+var accountName = "SBI";
+
+
+// noChange = 1201; // constants con't be changed 
+console.log(noChange);
+ peru = "ganesh";
+ accountName = "CBI";
+console.table([noChange,peru,accountName]);
+/*
+perfer not to use the var
+because of the issue in bolck scope and functional Scope .
+ */
---
+++ Basic_01/02_DataType.js
@@ -0,0 +1,16 @@
+
+
+//number range --> 2 to power 53;
+//bigint
+//String = " "
+//boolean = false/true;
+//null => stand alone problem .
+// undefine 
+//symbol => unique 
+
+
+// non primitive 
+// 1 Object 
+console.log(typeof null); // object 
+console.log(typeof undefined) // undefine
+
